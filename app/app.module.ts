9e5e1795baba3294c00bb51700bd4ba4a5dd8daa
Waleed Hammad <eng_walid2007@hotmail.com>
2017-08-12 12:32:57 +0300

    Add Modue, main and start component

---
+++ app/app.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {EventsAppComponent} from './events-app-component.js'
+@NgModule({
+    imports:[BrowserModule],
+    declarations:[EventsAppComponent],
+    bootstrap:[EventsAppComponent]
+})
+export class AppModule{}
---
+++ app/events-app-component.ts
@@ -0,0 +1,9 @@
+import{Component} from '@angular/core';
+
+@Component({
+    template:"<h1>Hello World Waleed  Hammad</h1>",
+    selector:"event-app"
+})
+export class EventsAppComponent{
+
+}
